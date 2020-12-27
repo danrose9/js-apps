fc5958c143f3fb77ef314c7aed16992c09031e36
Dan Rose <dan@reddog32.com>
2020-12-27 17:20:38 +0000

    initial

---
+++ basic-auth-page/JavaScriptSPA/authConfig.js
@@ -0,0 +1,21 @@
+const msalConfig = {
+    auth: {
+      clientId: "f4544630-60e8-470e-82c4-2aa42eedd071",
+      authority: "https://login.microsoftonline.com/common",
+      redirectUri: "http://localhost:3000/",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+
+  // Add here scopes for id token to be used at MS Identity Platform endpoints.
+  const loginRequest = {
+    scopes: ["openid", "profile", "User.Read"]
+  };
+
+  // Add here scopes for access token to be used at MS Graph API endpoints.
+  const tokenRequest = {
+    scopes: ["Mail.Read"]
+  };
---
+++ basic-auth-page/JavaScriptSPA/graphConfig.js
@@ -0,0 +1,5 @@
+const graphConfig = {
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+    graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages",
+    graphUsersEndpoint: "https://graph.microsoft.com/v1.0/users"
+  };
